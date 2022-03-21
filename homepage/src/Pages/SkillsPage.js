e4b4a53d6c41420e47dbf71fb0f2edae57fdab33
Avery Saastamoinen <93867381+averynova@users.noreply.github.com>
2022-03-21 09:44:14 +0200

    [Meta] Add title to skills page

--- homepage/src/Pages/SkillsPage.js
+++ homepage/src/Pages/SkillsPage.js
@@ -1,9 +1,14 @@
+import { Helmet } from "react-helmet";
+
 import Header from "../Components/Header";
 import Skills from "../Components/Skills";
 
 export default function SkillsPage() {
 	return (
 		<div className="SkillsPage">
+<Helmet>
+<title> Skills | Avery Saastamoinen </title>
+</Helmet>
 			<Header />
 			<Skills />
 		</div>
